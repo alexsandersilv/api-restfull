1b7d6d1519d35b82d60c700d6ccfbd2b82e8bb77
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-04-09 08:36:09 -0300

    Add Knex Migration

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,5 @@
 const path = require('path');
 
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
 
   development: {
@@ -12,39 +7,9 @@ module.exports = {
     connection: {
       filename: path.relative(__dirname, 'src', 'database', 'database.db')
     },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    },
     useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
-
 };
