8bc13925ba9a1ad6e50615df13a8c5b0febbed65
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-04-12 20:19:49 -0300

    knex tables

--- src/database/knex/migrations/20230407134447_createNotes.js
+++ src/database/knex/migrations/20230407134447_createNotes.js
@@ -2,22 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-
-  return knex.schema.createTable('notes', table => {
-    table.increments('id');
-    table.text('title');
-    table.text('description');
-    table.integer('user_id').references('id').inTable('users');
-    table.timestamps('created_at').defaultTo(knex.fn.now());
-    table.timestamps('updated_at').defaultTo(knex.fn.now());
-  });
-};
+exports.up = knex => knex.schema.createTable('notes', table => {
+  table.increments('id');
+  table.text('title');
+  table.text('description');
+  table.integer('user_id').references('id').inTable('users');
+  table.timestamps('created_at').defaultTo(knex.fn.now());
+  table.timestamps('updated_at').defaultTo(knex.fn.now());
+});
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.dropTable('notes');
-};
+exports.down = knex => knex.schema.dropTable('notes');
--- src/database/knex/migrations/20230409114917_createLinks.js
+++ src/database/knex/migrations/20230409114917_createLinks.js
@@ -2,21 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-
-  knex.schema.createTable('links', table => {
-    table.increments('id');
-    table.text('url').notNullable();
-    table.integer('note_id').references('id').inTable('notes').onDelete('CASCADE');
-    table.timestamps('created_at').defaultTo(knex.fn.now());
-  });
-
-};
+exports.up = knex => knex.schema.createTable('links', table => {
+  table.increments('id');
+  table.text('url').notNullable();
+  table.integer('note_id').references('id').inTable('notes').onDelete('CASCADE');
+  table.timestamps('created_at').defaultTo(knex.fn.now());
+});
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  knex.schema.dropTable('links');
-};
+exports.down = knex => knex.schema.dropTable('links');
